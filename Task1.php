52cb856abe9fa1d3e52597e6010e644e64f7056d
Tihomir Minev <tihomir.minev.sci@gmail.com>
2017-02-05 21:26:37 +0200

    AERT

--- Task1.php
+++ Task1.php
@@ -1,47 +1,47 @@
 <?php
 echo "задача 1: <br>";
 $mattrix = [];
-$num = 20;
-$row = 4;
-$col = 5;
-for($i=0; $i<$row; $i++){
+// $num = 20;
+// $row = 4;
+// $col = 5;
+// for($i=0; $i<$row; $i++){
 
-	for($j=0; $j<$col; $j++){
-		$mattrix[$i][$j] = $num;
-	$num--;		
-}
-}
-echo "<table border=1>";
-for ($m = 0; $m <$row; $m++){
-	echo "<tr>";
-	for($n=0; $n <$col; $n++){
+// 	for($j=0; $j<$col; $j++){
+// 		$mattrix[$i][$j] = $num;
+// 	$num--;		
+// }
+// }
+// echo "<table border=1>";
+// for ($m = 0; $m <$row; $m++){
+// 	echo "<tr>";
+// 	for($n=0; $n <$col; $n++){
 		
-		echo "<td>" . $mattrix[$m][$n]. "</td>";		
-	}
-	echo "</tr>";
-}
-echo "</table>";
+// 		echo "<td>" . $mattrix[$m][$n]. "</td>";		
+// 	}
+// 	echo "</tr>";
+// }
+// echo "</table>";
 
 
 
-echo "Задача 2: <br>";
-$furst = array();
-$sec = array();
-$num=1;
-echo "<table border=1>";
-for ($i=0; $i < 4 ; $i++) { 
-echo "<tr>";
-	for ($j=0; $j < 4; $j++) { 
+// echo "Задача 2: <br>";
+// $furst = array();
+// $sec = array();
+// $num=1;
+// echo "<table border=1>";
+// for ($i=0; $i < 4 ; $i++) { 
+// echo "<tr>";
+// 	for ($j=0; $j < 4; $j++) { 
 		
-		$furst[$i][$j]=$num;
+// 		$furst[$i][$j]=$num;
 		
-		echo "<td> " . $num . " </td>";
-		$num++;
-	}
-		$num = ($num*2)-1;
-echo "</tr>";
-}
-echo "</table>";
- echo "another task";
+// 		echo "<td> " . $num . " </td>";
+// 		$num++;
+// 	}
+// 		$num = ($num*2)-1;
+// echo "</tr>";
+// }
+// echo "</table>";
+//  echo "another task";
  
 
